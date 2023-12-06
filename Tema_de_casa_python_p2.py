0f52fdba658244507386f395b8221c5e09e454c7
raduionutgavrila <153100829+raduionutgavrila@users.noreply.github.com>
2023-12-06 18:29:17 +0200

    Tema_de_casa_python_p2.py

--- Tema_de_casa_python_p2.py
+++ Tema_de_casa_python_p2.py
@@ -6,13 +6,32 @@ df = pd.read_csv("data.csv")
 
 # Afișarea primelor X valori pentru toate coloanele
 X = 7
-print("Primele {7} valori:".format(X))
+print(f"Primele {X} valori:".format(X))
 print(df.head(X))
+
+plt.figure(figsize=(10, 5))
+plt.plot(df.index[:X], df['Durata'][:X], label='Durata', marker='|',ms= 20, color = 'b')
+plt.plot(df.index[:X], df['Puls'][:X], label='Puls', marker='|',ms = 20, color = 'r')
+plt.title(f'Primele {X} valori pentru Durata si Puls')
+plt.xlabel('Puls')
+plt.ylabel('Durata')
+plt.legend()
+plt.show()
+
 
 # Afișarea ultimelor Y valori pentru coloanele Durata și Puls
 Y = 9
-print("\nUltimele {9} valori pentru coloanele Durata și Puls:".format(Y))
+print(f"\nUltimele {Y} valori pentru coloanele Durata și Puls:".format(Y))
 print(df[['Durata', 'Puls']].tail(Y))
+
+plt.figure(figsize=(10, 5))
+plt.plot(df.index[-Y:], df['Durata'].tail(Y), label='Durata', marker='|',ms=20, color = 'b')
+plt.plot(df.index[-Y:], df['Puls'].tail(Y), label='Puls', marker='|',ms=20, color = 'r')
+plt.title(f'Ultimele {Y} valori pentru Durata si Puls')
+plt.xlabel('Puls')
+plt.ylabel('Durata')
+plt.legend()
+plt.show()
 
 # Plotează toate valorile pentru coloanele Durata și Puls
 plt.figure(figsize=(10, 6))
@@ -23,3 +42,4 @@ plt.xlabel('Index')
 plt.ylabel('Valoare')
 plt.legend()
 plt.show()
+
